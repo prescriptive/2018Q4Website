6af83bd377ec55ab54118d326d3032c31a98ef97
Alexander Fountain <alexander@digett.com>
2020-04-17 18:19:23 -0500

    phase 2

--- src/utils/linkResolver.js
+++ src/utils/linkResolver.js
@@ -1,13 +1,9 @@
 const linkResolver = doc => {
-  console.log(doc.type)
-  console.log(doc.uid)
   // Route for blog posts
   if (doc.type === "blog_post") {
     return "/blog/" + doc.uid
   }
-
   // Homepage route fallback
   return "/"
 }
-
 module.exports = linkResolver
