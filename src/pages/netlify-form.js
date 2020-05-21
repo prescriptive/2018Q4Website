09add2050d6f5c93512c3ff92c870e90bc4d1aeb
Alexander Fountain <alexander@digett.com>
2020-05-21 18:35:51 -0500

    resume test

--- src/pages/netlify-form.js
+++ src/pages/netlify-form.js
@@ -22,39 +22,94 @@ const NotFoundPage = () => (
     <Container>
       <Style404>
         <h1>Netlify Form</h1>
-        <form name="resumetest" method="POST" data-netlify="true">
-          <p>
-            <label>
-              Your Name: <input type="text" name="name" />
-            </label>
+
+        <form
+          name="resume2"
+          method="post"
+          netlify-honeypot="bot-field"
+          data-netlify="true"
+        >
+          <input type="hidden" name="form-name" value="contact3" />
+          <p hidden>
+            {" "}
+            <label htmlFor="bot-field">
+              Don’t fill this out: <input name="bot-field" />{" "}
+            </label>{" "}
           </p>
-          <p>
-            <label>
-              Your Email: <input type="email" name="email" />
+          <div class="form-group">
+            <label for="name" class="lb-name">
+              First Name *
             </label>
-          </p>
-          <p>
-            <label>
-              Your Role:{" "}
-              <select name="role[]" multiple>
-                <option value="leader">Leader</option>
-                <option value="follower">Follower</option>
-              </select>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              class="form-control"
+              data-required="true"
+              data-interactive="true"
+            />
+          </div>
+          <div class="form-group">
+            <label for="surname" class="lb-surname">
+              Last Name *
             </label>
-          </p>
-          <p>
-            <label>
-              Message: <textarea name="message"></textarea>
+            <input
+              type="text"
+              name="surname"
+              id="surname"
+              class="form-control"
+              data-required="true"
+              data-interactive="true"
+            />
+          </div>
+          <div class="form-group">
+            <label for="email" class="lb-email">
+              Email *
             </label>
-          </p>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              class="form-control"
+              data-required="true"
+              data-interactive="true"
+            />
+          </div>
+          <div class="form-group">
+            <label for="phone" class="lb-phone">
+              Phone Number
+            </label>
+            <input
+              type="tel"
+              name="phone"
+              id="phone"
+              class="form-control"
+              data-required="false"
+              data-interactive="true"
+            />
+          </div>
+          <div class="form-group text">
+            <label for="textarea" class="lb-message">
+              Message*
+            </label>
+            <textarea
+              name="textarea"
+              id="textarea"
+              class="textarea form-control"
+              data-required="true"
+              data-trim="true"
+            />
+          </div>
           <p>
             <label>
               Message: <input type="file" name="resume"></input>
             </label>
           </p>
-          <p>
-            <button type="submit">Send</button>
-          </p>
+          <div>
+            <button type="submit" class="btn btn-submit">
+              Send Message
+            </button>
+          </div>
         </form>
       </Style404>
     </Container>
