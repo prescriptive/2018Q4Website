7ecb619633fe7a7477b8ecb49e3f95d301fd1629
Alexander Fountain <alexander@digett.com>
2020-08-06 15:17:11 -0500

    podcast player styles

--- src/templates/podcast.js
+++ src/templates/podcast.js
@@ -13,8 +13,54 @@ import AudioPlayer from "react-h5-audio-player"
 import { RHAP_UI } from "react-h5-audio-player"
 import "react-h5-audio-player/lib/styles.css"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faTwitter } from "@fortawesome/free-brands-svg-icons"
-const PodcastStyle = styled.div``
+import { faRedoAlt } from "@fortawesome/free-solid-svg-icons"
+import { faUndoAlt } from "@fortawesome/free-solid-svg-icons"
+
+const PodcastStyle = styled.div`
+  .rhap_rewind-button {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    &:before {
+      content: "15";
+      color: ${variable.darkGray};
+      font-size: 10px;
+      position: absolute;
+    }
+    svg {
+      font-size: 34px;
+      color: ${variable.darkGray};
+    }
+  }
+  .rhap_forward-button {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    &:before {
+      content: "15";
+      color: black;
+      font-size: 10px;
+      position: absolute;
+      color: ${variable.darkGray};
+    }
+    svg {
+      font-size: 34px;
+      color: ${variable.darkGray};
+    }
+  }
+  .rhap_progress-bar-show-download {
+    background-color: ${variable.lightGray};
+  }
+  .rhap_download-progress {
+    background-color: ${variable.medGray};
+  }
+  .rhap_progress-filled {
+    background-color: ${variable.red};
+  }
+  .rhap_progress-indicator {
+    background: ${variable.red};
+  }
+`
 
 const Podcast = props => {
   // const prismicContent = props.data.prismic.allBlog_posts.edges[0]
@@ -38,8 +84,8 @@ const Podcast = props => {
           <h1>{node.title.text}</h1>
           <AudioPlayer
             progressJumpSteps={{
-              forward: 30000,
-              backward: 10000,
+              forward: 15000,
+              backward: 15000,
             }}
             layout="horizontal"
             customControlsSection={[RHAP_UI.VOLUME_CONTROLS]}
@@ -53,12 +99,12 @@ const Podcast = props => {
             autoPlay
             src={podcastUrl}
             onPlay={e => console.log("onPlay")}
-            CustomIcons={{
-              play: { faTwitter },
+            customIcons={{
+              rewind: <FontAwesomeIcon icon={faUndoAlt} />,
+              forward: <FontAwesomeIcon icon={faRedoAlt} />,
             }}
             // other props here
           />
-          {faTwitter}
           <RichText render={node.body.raw} linkResolver={linkResolver} />
         </Container>
       </PodcastStyle>
