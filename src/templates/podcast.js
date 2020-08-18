3f6fed7ba66244165f2c32eaea2ccaef53bfac08
Alexander Fountain <alexander@digett.com>
2020-08-18 18:44:16 -0500

    podcast query bottom

--- src/templates/podcast.js
+++ src/templates/podcast.js
@@ -13,9 +13,13 @@ import { Media, Player, controls } from "react-media-player"
 import AudioPlayer from "react-h5-audio-player"
 import "react-h5-audio-player/lib/styles.css"
 import BgImage from "../images/podcast.webp"
+import BgImageHat from "../images/podcasthat.png"
+import PodcastTeaser from "../components/entities/podcast/PodcastTeaser"
+
 const { PlayPause, MuteUnmute } = controls
 const PodHeader = styled.div`
   background-image: url(${BgImage});
+  background-size: cover;
   margin-bottom: 40px;
   .pod-header-container {
     min-height: 360px;
@@ -28,80 +32,140 @@ const PodHeader = styled.div`
   }
 `
 const PodcastStyle = styled.div`
-.pod-image{
-  img{
-    max-width:300px;
+  .pod-image {
+    img {
+      max-width: 300px;
+    }
   }
-}
-.pod-container{
-  display:flex;
-  justify-content:space-between;
-  .pod-left{
-    width:calc(50% - 20px);
+  .pod-container {
+    display: flex;
+    justify-content: space-between;
+    padding-bottom: 60px;
+    .pod-left {
+      width: calc(50% - 20px);
+    }
+    .pod-right {
+      width: calc(50% - 20px);
+      text-align: right;
+      img {
+        max-width: 100%;
+      }
+    }
   }
-  .pod-right{
-    width:calc(50% - 20px);
+  .pod-image-desc {
+    img {
+      float: left;
+      width: 200px;
+      margin: 0px 20px 10px 0px;
+    }
+    p {
+      display: unset;
+    }
   }
-}
-.pod-image-desc{
-  img{
-    float:left;
-    width:200px;
-    margin:0px 20px 10px 0px;
+  .pod-summary {
+    margin-top: 40px;
+    margin-bottom: 60px;
   }
-  p{
-    display:unset;
+  .podcasts-container {
+    display: flex;
+    justify-content: space-between;
+    > article {
+      width: calc(100% / 3 - 10px);
+    }
   }
-}
+  .rhap_rewind-button {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    &:before {
+      content: "15";
+      color: ${variable.darkGray};
+      font-size: 10px;
+      position: absolute;
+    }
+    svg {
+      font-size: 34px;
+      color: ${variable.darkGray};
+    }
+  }
+  .rhap_forward-button {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    &:before {
+      content: "15";
+      color: black;
+      font-size: 10px;
+      position: absolute;
+      color: ${variable.darkGray};
+    }
+    svg {
+      font-size: 34px;
+      color: ${variable.darkGray};
+    }
+  }
+  .rhap_progress-bar-show-download {
+    background-color: ${variable.lightGray};
+  }
+  .rhap_download-progress {
+    background-color: ${variable.medGray};
+  }
+  .rhap_progress-filled {
+    background-color: ${variable.red};
+  }
+  .rhap_progress-indicator {
+    background: ${variable.red};
+  }
 `
 const Podcast = props => {
-
   console.log(props)
 
   const podcastUrl = props.data.page.audio_url
-
+  const podcasts = props.data.podcast
   return (
     <Layout>
       {/* <SEO site={site} page={node} /> */}
       <PodHeader>
         <Container>
           <div className="pod-header-container">
-          <h1>{props.data.page.title}</h1>
+            <h1>{props.data.page.title}</h1>
           </div>
         </Container>
       </PodHeader>
       <PodcastStyle>
         <Container>
           <div className="pod-container">
-          <div className="pod-left">
-          <div className="pod-date">
-          {props.data.page.published_at}
+            <div className="pod-left">
+              <div className="pod-date">{props.data.page.published_at}</div>
+              <div className="pod-summary">{props.data.page.summary}</div>
+              <AudioPlayer
+                src={podcastUrl}
+                onPlay={e => console.log("onPlay")}
+              />
+              <h2>Show Notes</h2>
+              <div className="pod-image-desc">
+                <img src={props.data.page.artwork_url} />
+                <div
+                  className="pod-descs"
+                  dangerouslySetInnerHTML={{
+                    __html: props.data.page.description,
+                  }}
+                />
+              </div>
+            </div>
+            <div className="pod-right">
+              <img src={BgImageHat} />
+            </div>
           </div>
-          <div className="pod-summary">
-          {props.data.page.summary}
-          </div>
-          <AudioPlayer
-            src={podcastUrl}
-            onPlay={e => console.log("onPlay")}
-          />
-          <h2>Show Notes</h2>
-          <div className="pod-image-desc">
-  <img src={props.data.page.artwork_url} />
-            <div
-  className="pod-descs"
-  dangerouslySetInnerHTML={{ __html: props.data.page.description }}
-/>
-          </div>
-
-          </div>
-          <div className="pod-right">
-
-          </div>
-          </div>
-
-
 
           {/* <RichText render={props.data.page.description} linkResolver={linkResolver} /> */}
+        </Container>
+        <Container>
+          <div class="podcasts-container">
+            {podcasts.nodes.map((post, index) => (
+              <PodcastTeaser post={post} key={index}></PodcastTeaser>
+            ))}
+          </div>
         </Container>
       </PodcastStyle>
     </Layout>
@@ -130,7 +194,8 @@ export const podcastQuery = graphql`
         }
       }
     }
-    page: buzzsproutPodcastEpisode(id: { eq: $id }) {
+    podcast: allBuzzsproutPodcastEpisode {
+      nodes {
         artwork_url
         artist
         audio_url
@@ -141,6 +206,19 @@ export const podcastQuery = graphql`
         id
         published_at(formatString: "MMM D Y")
         tags
+      }
+    }
+    page: buzzsproutPodcastEpisode(id: { eq: $id }) {
+      artwork_url
+      artist
+      audio_url
+      description
+      summary
+      season_number
+      title
+      id
+      published_at(formatString: "MMM D Y")
+      tags
     }
   }
 `
