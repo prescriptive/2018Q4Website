f492d3ca2acb55e8810f1fc17c1074a66a44eb68
Alexander Fountain <alexander@digett.com>
2020-10-13 15:05:28 -0500

    filter private podcasts

--- gatsby-node.js
+++ gatsby-node.js
@@ -17,12 +17,13 @@ exports.createPages = async ({ graphql, actions }) => {
           }
         }
       }
-      buzz: allBuzzsproutPodcastEpisode(filter: { private: { eq: false } }) { {
+      buzz: allBuzzsproutPodcastEpisode(filter: { private: { eq: false } }) {
         nodes {
           id
           title
           slug
           buzzsproutId
+          private
         }
       }
     }
