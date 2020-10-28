db73e846fd72a6ad537ddd24aab15b23e2e9ae0f
Alexander Fountain <alexanderfountain@gmail.com>
2020-10-28 09:34:04 -0500

    force build

--- gatsby-config.js
+++ gatsby-config.js
@@ -76,6 +76,7 @@ module.exports = {
         },
       }
     },
+    
     `gatsby-plugin-preact`,
     // `gatsby-plugin-webpack-bundle-analyser-v2`,
     // 'gatsby-plugin-loadable-components-ssr',
