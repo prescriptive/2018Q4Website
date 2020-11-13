2d6f36dce7ea6170dd8cf023ac677bed5c8b7f69
Alexander Fountain <alexanderfountain@gmail.com>
2020-11-13 13:12:30 -0600

    removed typeform from html template

--- src/html.js
+++ src/html.js
@@ -46,13 +46,6 @@ export default function HTML(props) {
         />
         {props.postBodyComponents}
       </body>
-      <div
-        key={`body`}
-        id="___gatsby"
-        dangerouslySetInnerHTML={{
-          __html: `<a class="typeform-share button" href="https://form.typeform.com/to/LkNfiuv4" data-mode="popup" style="display:none;text-decoration:none;background-color:red;color:white;cursor:pointer;font-family:Helvetica,Arial,sans-serif;font-size:20px;line-height:50px;text-align:center;margin:0;height:50px;padding:0px 33px;border-radius:25px;max-width:100%;white-space:nowrap;overflow:hidden;text-overflow:ellipsis;font-weight:bold;-webkit-font-smoothing:antialiased;-moz-osx-font-smoothing:grayscale;" target="_blank"></a> <script> (function() { var qs,js,q,s,d=document, gi=d.getElementById, ce=d.createElement, gt=d.getElementsByTagName, id="typeform-cta", b="https://embed.typeform.com/"; if(!gi.call(d,id)){ js=ce.call(d,"script"); js.id=id; js.src=b+"embed.js"; q=gt.call(d,"script")[0]; q.parentNode.insertBefore(js,q) } })() </script>`,
-        }}
-      />
     </html>
   )
 }
