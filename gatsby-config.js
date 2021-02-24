6a960766a1b15b5b1214236b5052b8d872ccbb05
Alexander Fountain <alexander@digett.com>
2021-02-24 10:48:31 -0700

    uninstalled fortwaesome

--- gatsby-config.js
+++ gatsby-config.js
@@ -119,6 +119,36 @@ module.exports = {
         icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-favicon`,
+      options: {
+        logo: "./src/images/favicon.png",
+
+        // WebApp Manifest Configuration
+        appName: null, // Inferred with your package.json
+        appDescription: null,
+        developerName: null,
+        developerURL: null,
+        dir: "auto",
+        lang: "en-US",
+        background: "#fff",
+        theme_color: "#fff",
+        display: "standalone",
+        orientation: "any",
+        version: "1.0",
+
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: true,
+          yandex: false,
+          windows: false,
+        },
+      },
+    },
     `gatsby-plugin-preload-fonts`,
     {
       resolve: `gatsby-plugin-sitemap`,
