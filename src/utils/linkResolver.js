062a5878d4464308dd7f301fe3e59d85319b975a
Alexander Fountain <alexander@digett.com>
2021-04-23 16:03:20 -0500

    podcast added to link resolver

--- src/utils/linkResolver.js
+++ src/utils/linkResolver.js
@@ -6,6 +6,9 @@ const linkResolver = doc => {
   if (doc.type === "pa") {
     return "/" + doc.uid
   }
+  if (doc.type === "podcast") {
+    return "/podcast/" + doc.uid
+  }
 
   // Homepage route fallback
   return "/"
