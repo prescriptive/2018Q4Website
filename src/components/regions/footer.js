0d11756aa6083cd0805fa464dd5558abd32c66ad
Alexander Fountain <fountain@AlexandersMBP2.PK5001Z>
2021-04-28 10:36:45 -0500

    privacy policy

--- src/components/regions/footer.js
+++ src/components/regions/footer.js
@@ -2,6 +2,7 @@ import React from "react"
 import styled from "styled-components"
 import Container from "../container"
 import * as variable from "../variables"
+import { Link } from "gatsby"
 
 const FooterStyle = styled.footer`
   padding: 24px 0px;
@@ -10,6 +11,11 @@ const FooterStyle = styled.footer`
   color: white;
   div {
     color: white;
+    a{
+      color:white;
+      margin-top:10px;
+      display:block;
+    }
   }
 `
 
@@ -17,7 +23,8 @@ export const Footer = () => {
   return (
     <FooterStyle>
       <Container className="footer-container">
-        &copy; {new Date().getFullYear()} Prescriptive Data Solutions
+        <div>&copy; {new Date().getFullYear()} Prescriptive Data Solutions</div>
+        <div className="privacy-link"><Link to="/prescriptive-data-solutions-llc-privacy-policy">Privacy Policy</Link></div>
       </Container>
     </FooterStyle>
   )
