33aba1a682cecc1e8f2170127fedd43c0b7fce79
Alexander Fountain <alexander@digett.com>
2021-06-09 16:32:07 -0500

    red band and career updates

--- src/components/regions/footer.js
+++ src/components/regions/footer.js
@@ -2,6 +2,7 @@ import React from "react"
 import styled from "styled-components"
 import Container from "../container"
 import * as variable from "../variables"
+import { Link } from "gatsby"
 
 const FooterStyle = styled.footer`
   padding: 24px 0px;
@@ -10,6 +11,9 @@ const FooterStyle = styled.footer`
   color: white;
   div {
     color: white;
+    a{
+      color:white;
+    }
   }
 `
 
@@ -17,7 +21,12 @@ export const Footer = () => {
   return (
     <FooterStyle>
       <Container className="footer-container">
+      <div>
         &copy; {new Date().getFullYear()} Prescriptive Data Solutions
+        </div>
+        <div>
+        <Link to="/prescriptive-data-solutions-llc-privacy-policy">Privacy Policy</Link>
+        </div>
       </Container>
     </FooterStyle>
   )
