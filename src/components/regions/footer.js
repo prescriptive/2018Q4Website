6e2ebb47afab83a82756bb1f91e68d1d0af246c3
Alexander Fountain <alexander@digett.com>
2021-06-10 13:06:24 -0500

    home merged into master

--- src/components/regions/footer.js
+++ src/components/regions/footer.js
@@ -2,14 +2,19 @@ import React from "react"
 import styled from "styled-components"
 import Container from "../container"
 import * as variable from "../variables"
+import { Link } from "gatsby"
 
 const FooterStyle = styled.footer`
   padding: 24px 0px;
   background-color: ${variable.darkGray};
   text-align: center;
   color: white;
+  line-height: 24px;
   div {
     color: white;
+    a{
+      color:white;
+    }
   }
 `
 
@@ -17,7 +22,12 @@ export const Footer = () => {
   return (
     <FooterStyle>
       <Container className="footer-container">
+      <div>
         &copy; {new Date().getFullYear()} Prescriptive Data Solutions
+        </div>
+        <div>
+        <Link to="/prescriptive-data-solutions-llc-privacy-policy">Privacy Policy</Link>
+        </div>
       </Container>
     </FooterStyle>
   )
